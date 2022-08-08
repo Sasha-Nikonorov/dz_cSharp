97bc759edb41c0198cf8281412a9ce7a3aae87cd
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-08 21:11:55 +0300

    create program a<b>c

---
+++ dz_z4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberAString = Console.ReadLine();
+int numberA = int.Parse(numberAString!);
+
+Console.WriteLine("Пожалуйста, введите целое число");
+string? numberBString = Console.ReadLine();
+int numberB = int.Parse(numberBString!);
+
+Console.WriteLine("Пожалуйста, введите целое число");
+string? numberCString = Console.ReadLine();
+int numberC = int.Parse(numberCString!);
+
+int max = numberA;
+
+if (numberA > max ) max = numberA;
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
