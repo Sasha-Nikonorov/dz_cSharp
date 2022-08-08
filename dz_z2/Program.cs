1fa5870004ef6e6db01a3e99102730a0811ba705
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-08 21:00:41 +0300

    create program a>b

---
+++ dz_z2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberAString = Console.ReadLine();
+int numberA = int.Parse(numberAString!);
+
+Console.WriteLine("Пожалуйста, введите целое число");
+string? numberBString = Console.ReadLine();
+int numberB = int.Parse(numberBString!);
+
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
+if(numberA == numberB)
+{
+    Console.WriteLine("Веденные числа не отличаются");
+}
+
