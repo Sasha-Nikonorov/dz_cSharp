395f96c93ef704ec4d1dde43812d466e73777d49
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-09 10:12:29 +0300

    create even numbers program

---
+++ dz_z6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberAString = Console.ReadLine();
+int numberA = int.Parse(numberAString!);
+
+int numberB = numberA / 2;
+
+int numberC = numberB * 2;
+
+if(numberA == numberC)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
