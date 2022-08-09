908236c84301eb01c14b306ccb508ff9ea859091
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-09 10:43:01 +0300

    create program numbers from 1 to N

---
+++ dz_z8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+string? numberAString = Console.ReadLine();
+int numberA = int.Parse(numberAString!);
+
+int count = 1;
+
+while(count < numberA)
+{
+    if (count%2==0)
+    {
+    Console.WriteLine(count);
+    }
+    count = count + 1;
+}
